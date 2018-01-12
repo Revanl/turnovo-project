632240f09857cf571a1d62a546576c2355014734
Iani Garfalski <ianihrg@abv.bg>
2018-01-12 13:40:56 +0200

    Update routes.php

--- turnovo/routes.php
+++ turnovo/routes.php
@@ -7,4 +7,12 @@ Route::set('create.php', function(){
 	$controller = new IndexController();
 	$controller->create();
 });
+Route::set('edit.php', function(){
+	$controller = new IndexController();
+	$controller->edit();
+});
+Route::set('destroy.php', function(){
+	$controller = new IndexController();
+	$controller->destroy();
+});
 ?>
